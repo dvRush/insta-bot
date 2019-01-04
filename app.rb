6c7bb647964bd6b7eacce3b58f74c96f155f9523
Rafael Belo <rafaelrpbelo@gmail.com>
2019-01-04 17:42:06 -0300

    Create a minimal crawler app

--- app.rb
+++ app.rb
@@ -1,1 +1,39 @@
-puts 'Hello'
+require 'pry'
+require 'capybara/dsl'
+require 'selenium-webdriver'
+require 'chromedriver/helper'
+
+Chromedriver.set_version "2.35"
+Capybara.register_driver(:headless_chrome) do |app|
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    desired_capabilities: {
+      chromeOptions: {
+        args: %w[headless disable-gpu no-sandbox]
+      }
+    }
+  )
+end
+
+Capybara.configure do |config|
+  config.default_driver    = :headless_chrome
+  config.javascript_driver = :headless_chrome
+  config.default_max_wait_time = 5
+end
+
+class MyDriver
+  include Capybara::DSL
+
+  def start
+    visit 'http://example.com'
+
+    page.driver.save_screenshot('./ss.png')
+  end
+
+  def self.start
+    new.start
+  end
+end
+
+MyDriver.start
